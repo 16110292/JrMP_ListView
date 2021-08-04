e7f13122348de06f2611ca87984e8c6920832f20
Farendy Naufal <farendy.asyikin@students.amikom.ac.id>
2021-08-04 22:12:44 +0700

    Part 2

--- app/src/main/java/com/example/jrmplistview/MainActivity.java
+++ app/src/main/java/com/example/jrmplistview/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.jrmplistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView lvItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
