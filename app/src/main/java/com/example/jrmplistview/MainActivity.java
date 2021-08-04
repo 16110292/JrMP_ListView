9dea64fef7cee7b5f867e937fff6f0285f58b79c
Farendy Naufal <farendy.asyikin@students.amikom.ac.id>
2021-08-04 22:29:13 +0700

    Part 2

--- app/src/main/java/com/example/jrmplistview/MainActivity.java
+++ app/src/main/java/com/example/jrmplistview/MainActivity.java
@@ -7,9 +7,15 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private ListView lvItems;
+    private String [] namanegara=new String[] {"Indonesia", "Malaysia", "Singapore", "Thailand"}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("ListView Sederhana");
+        getSupportActionBar().setTitle("JrMP List View");
+        lvItems=findViewById(R.id.lv);
+
+
     }
 }
